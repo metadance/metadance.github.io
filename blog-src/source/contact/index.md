73e51c5b87bb1dcc87cdb7ca781016bf63121560
Piotr Trocki <piotr.trocki@callstack.com>
2019-11-26 14:07:06 +0100

    init Gatsby

--- blog-src/source/contact/index.md
+++
@@ -1,7 +0,0 @@
-title: Kontakt
-date: 2015-08-22 19:45:59
-comments: false
----
-
-Pytania i sugestie prosze kierowac na adress
-##### *info(at)kochamtaniec.pl*
---
+++ config.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  url: 'http://kochamtaniec.pl/blog/',
+  pathPrefix: '/',
+  title: 'Kocham Taniec',
+  subtitle: 'Your daily dance feed',
+  copyright: '',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: '',
+      path: '/'
+    },
+  ],
+  author: {
+    name: 'Piotr Trocki',
+    photo: '/avatar.jpg',
+    bio: 'Passionate about dancing. Likes discover new styles and take part in dance festivals',
+    contacts: {
+      email: 'info@kochamtaniec.pl',
+      facebook: 'https://www.facebook.com/kochamtaniecpl/',
+      telegram: '',
+      twitter: '',
+      github: '',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: ''
+    }
+  }
+};
